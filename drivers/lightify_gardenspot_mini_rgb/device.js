18c0e42e8da0a14e400e755918f97edb86ea6abb
robinbolscher@gmail.com <robinbolscher@gmail.com>
2017-10-05 19:18:47 +0200

    Version bump (v1.2.0): add support for Gardenspot Mini RGB

---
+++ drivers/lightify_gardenspot_mini_rgb/device.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const ZigBeeLightDevice = require('homey-meshdriver').ZigBeeLightDevice;
+
+class LightifyGardenspotMiniRGBZigBee extends ZigBeeLightDevice {
+
+}
+
+module.exports = LightifyGardenspotMiniRGBZigBee;
